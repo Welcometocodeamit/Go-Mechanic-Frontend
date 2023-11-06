5816ec53997bdf0ae795f92bf14371cb9a6eca75
Welcometocodeamit <kumbharamit98@gmail.com>
2023-11-06 17:55:32 +0530

    Ham nav

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,24 +1,79 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import { useState } from 'react';
+
 
 export default function Navbar() {
+  const [mobileMenuVisible, setMobileMenuVisible] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuVisible(!mobileMenuVisible);
+  };
+
   return (
-    <div className='navbar-a'>
-      <div className='leftNav-a'>
+    <div className="navbar-a">
+      <div className="leftNav-a">
         <li>
-            <Link to='/'><ul>Go-Mechanic</ul></Link>
+          <Link to="/">
+            <ul>Go-Mechanic</ul>
+          </Link>
         </li>
       </div>
+      {/* Hamburger menu */}
+      <div className={`navbar ${mobileMenuVisible ? 'mobile-menu-open' : ''}`}>
+        <div className="hamburger-menu" onClick={toggleMobileMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
 
-      <div className='rightNav-a'>
-        <li>
-            <Link to='/services'><ul>Garages</ul></Link>
-            <Link to='/vehicles'><ul>Vehicles</ul></Link>
-            <Link to='/aboutus'><ul>About us</ul></Link>
-            <Link to='/login'><ul>Log in</ul></Link>
-        </li>
-
+        <div className="rightNav-a">
+          <li>
+            <Link to="/services">
+              <ul>Garages</ul>
+            </Link>
+            <Link to="/vehicles">
+              <ul>Vehicles</ul>
+            </Link>
+            <Link to="/aboutus">
+              <ul>About us</ul>
+            </Link>
+            <Link to="/login">
+              <ul>Log in</ul>
+            </Link>
+          </li>
+        </div>
       </div>
     </div>
-  )
+  );
 }
+
+// export default function Navbar() {
+
+//   return (
+//     <div className='navbar-a'>
+//        <div className='leftNav-a'>
+//           <li>
+//               <Link to='/'><ul>Go-Mechanic</ul></Link>
+//           </li>
+//         </div>
+// {/* Hamburger menu */}
+//       <div className='navbar'>
+//         <div class="hamburger-menu">
+//               <div class="bar"></div>
+//               <div class="bar"></div>
+//               <div class="bar"></div>
+//           </div>
+       
+//         <div className='rightNav-a'>
+//           <li>
+//               <Link to='/services'><ul>Garages</ul></Link>
+//               <Link to='/vehicles'><ul>Vehicles</ul></Link>
+//               <Link to='/aboutus'><ul>About us</ul></Link>
+//               <Link to='/login'><ul>Log in</ul></Link>
+//           </li>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
