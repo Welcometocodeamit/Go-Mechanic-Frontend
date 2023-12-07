b807644def709489be04f20dbc2c41da6d2f728b
Amit <kumbharamit98@gmail.com>
2023-12-07 12:56:46 +0530

    about me commented

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
         <Route path='/adminLogin' element={<AdminLogin/>}/>
         <Route path='/updatevehicle' element={<UpdateVehicleForm/>}/>
       </Routes>
-      <AboutMe/>
+      {/* <AboutMe/> */}
    </BrowserRouter>
    </>
   );
